9221b88d136e6b0762e228bba3a39151d16bf8ed
towakey <kyosgame@gmail.com>
2020-02-20 00:54:04 +0900

    あってるかは分からないがとりあえず繋げられる状態（たぶん抜ける条件が足りないと思う）

--- letspokemonshiritori.py
+++ letspokemonshiritori.py
@@ -1,82 +1,100 @@
 # coding: utf-8
 import json
 
-pokedex_path = 'pokedex.json'
+def main():
+    pokedex_path = 'pokedex.json'
 
-with open(pokedex_path,encoding='utf-8') as f:
-    pokedex = json.load(f)
+    with open(pokedex_path,encoding='utf-8') as f:
+        pokedex = json.load(f)
 
 
-# print(pokedex[0]['name'][0])
+    # print(pokedex[0]['name'][0])
 
-shiritori = {}
-for word in pokedex:
-    set_pokemon = {}
+    shiritori = {}
+    for word in pokedex:
+        set_pokemon = {}
 
-    # 基本的には最後の文字を使用する
-    lastplace = -1
+        # 基本的には最後の文字を使用する
+        lastplace = -1
 
-    # ニドラン♂、ニドラン♀の時
-    if word['name'][lastplace] == '♂' or word['name'][lastplace] == '♀':
-        set_pokemon['lastword'] = 'ス'
-    # ポリゴン2の時
-    elif word['name'][lastplace] == '2':
-        set_pokemon['lastword'] = 'ツ'
-    # ポリゴンZの時
-    elif word['name'][lastplace] == 'Z':
-        set_pokemon['lastword'] = 'ト'
-    else:
-        # 最後の文字が長音の時は、直前の文字を使用する
-        if word['name'][lastplace] == 'ー':
-            lastplace = -2
-        # カタカナの小文字は大文字にする
-        if word['name'][lastplace] == "ァ":
-            set_pokemon['lastword'] = "ア"
-        elif word['name'][lastplace] == "ィ":
-            set_pokemon['lastword'] = "イ"
-        elif word['name'][lastplace] == "ゥ":
-            set_pokemon['lastword'] = "ウ"
-        elif word['name'][lastplace] == "ェ":
-            set_pokemon['lastword'] = "エ"
-        elif word['name'][lastplace] == "ォ":
-            set_pokemon['lastword'] = "オ"
-        elif word['name'][lastplace] == "ッ":
-            set_pokemon['lastword'] = "ツ"
-        elif word['name'][lastplace] == "ャ":
-            set_pokemon['lastword'] = "ヤ"
-        elif word['name'][lastplace] == "ュ":
-            set_pokemon['lastword'] = "ユ"
-        elif word['name'][lastplace] == "ョ":
-            set_pokemon['lastword'] = "ヨ"
-        elif word['name'][lastplace] == "ヮ":
-            set_pokemon['lastword'] = "ワ"
-        elif word['name'][lastplace] == "ヵ":
-            set_pokemon['lastword'] = "カ"
-        elif word['name'][lastplace] == "ヶ":
-            set_pokemon['lastword'] = "ケ"
+        # ニドラン♂、ニドラン♀の時
+        if word['name'][lastplace] == '♂' or word['name'][lastplace] == '♀':
+            set_pokemon['lastword'] = 'ス'
+        # ポリゴン2の時
+        elif word['name'][lastplace] == '2':
+            set_pokemon['lastword'] = 'ツ'
+        # ポリゴンZの時
+        elif word['name'][lastplace] == 'Z':
+            set_pokemon['lastword'] = 'ト'
         else:
-            # それ以外の場合はそのまま使用する
-            set_pokemon['lastword'] = word['name'][lastplace]
-    # フラグのデフォルト値はFalse
-    set_pokemon['flag'] = False
+            # 最後の文字が長音の時は、直前の文字を使用する
+            if word['name'][lastplace] == 'ー':
+                lastplace = -2
+            # カタカナの小文字は大文字にする
+            if word['name'][lastplace] == "ァ":
+                set_pokemon['lastword'] = "ア"
+            elif word['name'][lastplace] == "ィ":
+                set_pokemon['lastword'] = "イ"
+            elif word['name'][lastplace] == "ゥ":
+                set_pokemon['lastword'] = "ウ"
+            elif word['name'][lastplace] == "ェ":
+                set_pokemon['lastword'] = "エ"
+            elif word['name'][lastplace] == "ォ":
+                set_pokemon['lastword'] = "オ"
+            elif word['name'][lastplace] == "ッ":
+                set_pokemon['lastword'] = "ツ"
+            elif word['name'][lastplace] == "ャ":
+                set_pokemon['lastword'] = "ヤ"
+            elif word['name'][lastplace] == "ュ":
+                set_pokemon['lastword'] = "ユ"
+            elif word['name'][lastplace] == "ョ":
+                set_pokemon['lastword'] = "ヨ"
+            elif word['name'][lastplace] == "ヮ":
+                set_pokemon['lastword'] = "ワ"
+            elif word['name'][lastplace] == "ヵ":
+                set_pokemon['lastword'] = "カ"
+            elif word['name'][lastplace] == "ヶ":
+                set_pokemon['lastword'] = "ケ"
+            else:
+                # それ以外の場合はそのまま使用する
+                set_pokemon['lastword'] = word['name'][lastplace]
+        # フラグのデフォルト値はFalse
+        set_pokemon['flag'] = False
 
-    # 頭文字の配列が無ければ連想配列として初期化
-    if word['name'][0] not in shiritori:
-        shiritori[word['name'][0]] = {}
-    # ポケモンの名前で配列を保存
-    shiritori[word['name'][0]][word['name']] = set_pokemon
+        # 頭文字の配列が無ければ連想配列として初期化
+        if word['name'][0] not in shiritori:
+            shiritori[word['name'][0]] = {}
+        # ポケモンの名前で配列を保存
+        shiritori[word['name'][0]][word['name']] = set_pokemon
 
-# print(shiritori['ア'])
+    shiritori_list = []
+    shiritori_max = []
+    shiritori_word = "ア"
+    cnt = 0
+    flag = shiritori_run(shiritori,shiritori_word,shiritori_list,True,shiritori_max)
 
-shiritori_word = "ア"
-shiritori_flag = True
-while shiritori_flag:
-    # print(shiritori[start_word].keys)
-    for word in shiritori[shiritori_word].values():
-        print(word.keys)
-        if not word['flag']:
-            shiritori_word = word['lastword']
+def shiritori_run(shiritori,shiritori_word,shiritori_list,flag,shiritori_max):
+    # 頭文字のポケモンがいるか検査する
+    if shiritori_word in shiritori:
+        for key,word in shiritori[shiritori_word].items():
+            if word['flag'] == False:
+                shiritori[shiritori_word][key]['flag'] = True
+                # shiritori_word = word['lastword']
+                shiritori_list.append(key)
+                flag = shiritori_run(shiritori,word['lastword'],shiritori_list,flag,shiritori_max)
+                # もし返ってきたらFalseになっていたら…
+                if flag == False:
+                    # 現在の状態を保存
+                    shiritori_max = shiritori_list[:]
+                    # 最後の名前を削除
+                    del shiritori_list[-1]
+                    # フラグを戻す
+                    shiritori[shiritori_word][key]['flag'] = False
 
-        print(word)
-    # shiritori_flag = False
+    else:
+        flag = False
+    return flag
+5
 
+if __name__ == "__main__":
+    main()
